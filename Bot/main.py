aabb0e1d22a3257f526038244994a5225025ecc7
tiwprash <tprashant232@gmail.com>
2024-09-15 17:26:46 +0530

    neww revision

--- Bot/main.py
+++ Bot/main.py
@@ -109,12 +109,12 @@ def bb_statergy(symbol, interval,signals):
         return signals
 
     try:
-        if df['close'].iloc[-2] >= df['bb_upper'].iloc[-2] and bb_difference_perc >= 1.5  and df['mfi'].iloc[-2] >= 75 and df['rsi'].iloc[-2] >= 85 and volume24 >= 2000000 and df['volume'].iloc[-2] < df['volume'].iloc[-3]:   
+        if df['close'].iloc[-2] >= df['bb_upper'].iloc[-2] and bb_difference_perc >= 1.5  and df['mfi'].iloc[-2] >= 75 and df['rsi'].iloc[-2] >= 92 and volume24 >= 2000000 :   
             signals.append({"close_price":df['close'].iloc[-2],"Symbol": symbol, "Signal": "sell", "Percentage": bb_difference_perc, "MFI": df['mfi'].iloc[-2], "TP": tp_for_short, "SL": sl_for_short,"RSI":df['rsi'].iloc[-2]})
             logging.info(f"{symbol} sell signal generated")
             print(f"{symbol} sell || {signals[-1]} || {df['rsi'].iloc[-2]} || {df['rsi'].iloc[-2]}")
 
-        elif df['close'].iloc[-2] <= df['bb_lower'].iloc[-2] and bb_difference_perc >= 1.5 and df['mfi'].iloc[-2] <= 25 and df['rsi'].iloc[-2] <= 15 and volume24 >= 2000000 and df['volume'].iloc[-2] < df['volume'].iloc[-3]:
+        elif df['close'].iloc[-2] <= df['bb_lower'].iloc[-2] and bb_difference_perc >= 1.5 and df['mfi'].iloc[-2] <= 25 and df['rsi'].iloc[-2] <= 8 and volume24 >= 2000000 :
             signals.append({"close_price":df['close'].iloc[-2],"Symbol": symbol, "Signal": "buy", "Percentage": bb_difference_perc, "MFI": df['mfi'].iloc[-2], "TP": tp_for_long, "SL": sl_for_long,"RSI":df['rsi'].iloc[-2]})
             logging.info(f"{symbol} buy signal generated")
             print(f"{symbol} buy || {signals[-1]} || {df['rsi'].iloc[-2]} || {df['rsi'].iloc[-2]}")
